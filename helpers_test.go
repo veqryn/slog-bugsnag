0b75b69b589751eb261022dc633962911e439924
Chris Duncan <veqryn@hotmail.com>
2023-12-09 02:47:02 -0700

    Update tests and log to bug converter

--- helpers_test.go
+++ helpers_test.go
@@ -10,9 +10,9 @@ import (
 var defaultTime = time.Date(2023, 9, 29, 13, 0, 59, 0, time.UTC)
 
 type testHandler struct {
-	Ctx    context.Context
-	Record slog.Record
-	source bool
+	Ctx     context.Context
+	Records []slog.Record
+	source  bool
 }
 
 func (h *testHandler) Enabled(context.Context, slog.Level) bool {
@@ -21,8 +21,8 @@ func (h *testHandler) Enabled(context.Context, slog.Level) bool {
 
 func (h *testHandler) Handle(ctx context.Context, r slog.Record) error {
 	h.Ctx = ctx
-	h.Record = r
-	h.Record.Time = defaultTime
+	r.Time = defaultTime
+	h.Records = append(h.Records, r)
 	return nil
 }
 
@@ -34,18 +34,34 @@ func (h *testHandler) WithAttrs([]slog.Attr) slog.Handler {
 	panic("shouldn't be called")
 }
 
+// String of latest record
 func (h *testHandler) String() string {
+	return h.string(len(h.Records) - 1)
+}
+
+func (h *testHandler) string(recordIndex int) string {
+	if len(h.Records) == 0 {
+		return ""
+	}
 	buf := &bytes.Buffer{}
-	err := slog.NewTextHandler(buf, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: h.source}).Handle(context.Background(), h.Record)
+	err := slog.NewTextHandler(buf, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: h.source}).Handle(context.Background(), h.Records[recordIndex])
 	if err != nil {
 		panic(err)
 	}
 	return buf.String()
 }
 
+// MarshalJSON latest record
 func (h *testHandler) MarshalJSON() ([]byte, error) {
+	return h.marshalJSON(len(h.Records) - 1)
+}
+
+func (h *testHandler) marshalJSON(recordIndex int) ([]byte, error) {
+	if len(h.Records) == 0 {
+		return nil, nil
+	}
 	buf := &bytes.Buffer{}
-	err := slog.NewJSONHandler(buf, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: h.source}).Handle(context.Background(), h.Record)
+	err := slog.NewJSONHandler(buf, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: h.source}).Handle(context.Background(), h.Records[recordIndex])
 	if err != nil {
 		return nil, err
 	}
